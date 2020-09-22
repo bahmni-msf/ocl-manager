2ec7afce0172d2af17993fc5071a188a3e92dbe8
Michaël Bontyes <michaelbontyes@gmail.com>
2020-09-22 11:17:05 -0400

    Adding fr,en,ht,es,ar to supported locales

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 CT="Content-Type:application/json"
 AUTH="Authorization: Token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-OCL_API_HOST="http://localhost:8000"
+OCL_API_HOST="https://api.openconceptlab.org"
 org="@org.json"
 source="@source.json"
 
@@ -12,7 +12,7 @@ echo ${CREATE_ORG}
 echo "_____________________________________"
 
 echo "Create a Source.."
-CREATE_SOURCE=`curl -s -H "${AUTH}" ${OCL_API_HOST}/orgs/MSF/sources/ -X POST -H "${CT}" -d $source`
+CREATE_SOURCE=`curl -s -H "${AUTH}" ${OCL_API_HOST}/orgs/MSFOCP/sources/ -X POST -H "${CT}" -d $source`
 echo ${CREATE_SOURCE}
 echo "_____________________________________"
 
@@ -22,8 +22,8 @@ while read user; do
    echo "Create user -  ${user}"
    CREATE_USER=`curl -s -H "${AUTH}" ${OCL_API_HOST}/users/ -X POST -H "${CT}" -d "{ \"username\":\"${user}\",\"email\":\"test@test.com\",\"name\":\"${user}\"}"`
    echo ${CREATE_USER}
-   echo "Add user - ${user} as a member of MSF organization"
-   ADD_USER=`curl -s -H "${AUTH}" ${OCL_API_HOST}/orgs/MSF/members/${user}/ -X PUT`
+   echo "Add user - ${user} as a member of MSFOCP organization"
+   ADD_USER=`curl -s -H "${AUTH}" ${OCL_API_HOST}/orgs/MSFOCP/members/${user}/ -X PUT`
    echo ${ADD_USER}
    echo "_____________________________________"
 done
